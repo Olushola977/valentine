605f739a49c724edb4686cf0a77f29bd5f8f01a7
Olushola977 <adeyeyeolushola977@gmail.com>
2024-02-06 00:28:53 +0100

    responsiveness

--- app/val-response/fail/page.js
+++ app/val-response/fail/page.js
@@ -1,12 +1,12 @@
 export default function Home() {
   return (
-    <main className="flex h-screen w-full items-center justify-center bg-blue-200">
+    <main className="flex h-full w-full items-center justify-center bg-blue-200">
         <div className="flex flex-col md:flex-row align-center items-center w-full">
-          <div className="w-1/2 p-3">
+          <div className="w-full md:w-1/2 p-5 md:p-3 order-2">
           <p className="text-white text-2xl">I respect your decision, though it feels like a gentle rain on the petals of my hopeful heart. Wishing you a Valentine&apos;s filled with happiness, even if not by my side. 💔☔️</p>
           </div>
           
-          <div className="w-1/2">
+          <div className="w-full md:w-1/2 order-1">
             <img
               alt="image"
               src={"https://media1.tenor.com/m/LuZ_0ZWFMAkAAAAC/sad-crying.gif"}
--- app/val-response/success/page.js
+++ app/val-response/success/page.js
@@ -2,13 +2,13 @@ export default function Home() {
   const numberOfBalloons = 10;
 
   return (
-    <main className="flex h-screen w-full items-center justify-center bg-love">
+    <main className="flex h-full w-full items-center justify-center bg-love">
         <div className="flex flex-col md:flex-row align-center items-center w-full h-full">
-          <div className="w-1/2 p-3">
+          <div className="w-full md:w-1/2 p-5 md:p-3 order-2">
           <p className="text-white text-2xl">Your &apos;yes&apos; has painted the skies with the hues of romance. Excited for a Valentine&apos;s Day filled with love, laughter, and endless joy with you by my side. 💖</p>
           </div>
           
-          <div className="w-1/2 h-full">
+          <div className="w-full md:w-1/2 order-1">
             <img
               alt="image"
               src={"https://media1.tenor.com/m/PweWUNx9saMAAAAC/love-friends.gif"}
